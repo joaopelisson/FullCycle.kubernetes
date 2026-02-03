7b6a605d3c3be8fbcd38992d764a1e3e53a2013b
joaopelisson <joaopedro.devbr@gmail.com>
2026-02-03 19:43:30 -0300

    docs: update comments to clarify server functionality and objective

--- server.go
+++ server.go
@@ -2,6 +2,11 @@ package main
 
 import "net/http"
 
+/**
+* Example of a simple Go web server that responds with "Hello, World!" to any HTTP request.
+* Objective: Kubernetes deployment example.
+**/
+
 func main() {
 	http.HandleFunc("/", Hello)
 	http.ListenAndServe(":80", nil)
